b4561d59d980ca2048a634675981929e092b4756
Francesco Abbate <francesco.bbt@gmail.com>
2015-02-02 11:20:02 +0100

    Make a specific function to build linest matrix on discrete factors
    The function buildFactorMatrix create a matrix that correspond to
    tX * X where X is the linear model matrix. It works in the case there
    are only factors with discrete levels and use the countOccurrences
    function. The second argument "factors" of the functions specify a list
    of the factors that should be used to build the matrix.

--- dataframe.js
+++ dataframe.js
@@ -41,36 +41,39 @@ function countOccurrences(t, values) {
 var t = DataFrame.create(3, 2, ['boo', 3, 'foo', 3.14, 'data', -1.15]);
 var sige = DataFrame.create(1020, 9, cpm_sige_data)
 
-var factors = [
+function buildFactorMatrix(tab, factors) {
+    var Kd = [];
+    for (var p = 0; p < factors.length; p++) {
+        var Krow = [];
+        for (var q = 0; q < factors.length; q++) {
+            var s = 0;
+            if (p === q) {
+                s = countOccurrences(tab, factors[p]);
+            } else {
+                var cf = [].concat(factors[p]).concat(factors[q]);
+                s = countOccurrences(tab, cf);
+            }
+            Krow.push(s);
+        }
+        Kd.push(Krow);
+    }
+    return Sylvester.Matrix.create(Kd);
+}
+
+var cpm_factors = [
     [{column: 4, value: 'QFX1001'}, {column: 5, value: 1}]
 ];
 
 for (var site = 2; site <= 17; site++) {
-    factors.push([{column: 5, value: site}]);
+    cpm_factors.push([{column: 5, value: site}]);
 }
 
 var tools = ['QFX1001', 'QFX1002', 'QFX1003', 'QFX1006']
 for (var k = 1; k < tools.length; k++) {
-    factors.push([{column: 4, value: tools[k]}]);
+    cpm_factors.push([{column: 4, value: tools[k]}]);
 }
 
-var Kd = [];
-for (var p = 0; p < factors.length; p++) {
-    var Krow = [];
-    for (var q = 0; q < factors.length; q++) {
-        var s = 0;
-        if (p === q) {
-            s = countOccurrences(sige, factors[p]);
-        } else {
-            var cf = [].concat(factors[p]).concat(factors[q]);
-            s = countOccurrences(sige, cf);
-        }
-        Krow.push(s);
-    }
-    Kd.push(Krow);
-}
-
-var K = Sylvester.Matrix.create(Kd);
-console.log(K.inverse().inspect())
-console.log("count:", countOccurrences(sige, factors[17]));
+var K = buildFactorMatrix(sige, cpm_factors);
+console.log(K.inspect());
+console.log("count:", countOccurrences(sige, cpm_factors[17]));
 console.log(">>", t.e(2,1), t.e(2, 2));
