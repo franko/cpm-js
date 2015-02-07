7eaec9380c9d6dde925f573ab3359bf4522c5a97
francesco <francesco.bbt@gmail.com>
2015-02-07 22:05:00 +0100

    Implement code to parse Spectra FX result files

--- dataframe.js
+++ dataframe.js
@@ -184,3 +184,91 @@ var cpm_test = function() {
         console.log(stat.e(i, 1), stat.e(i, 2))
     }
 };
+
+var list_tonumber = function(row) {
+    for (var i = 0; i < row.length; i++) {
+        var x = Number(row[i]);
+        if (!isNaN(x) && row[i] != "") {
+            row[i] = x;
+        }
+    }
+};
+
+function csvReader(text) {
+    var lines = text.split("\n");
+    var i = 0;
+    var next = function() {
+        if (i < lines.length) {
+            var row = lines[i++].replace("\r", "").split(",");
+            list_tonumber(row);
+            return row;
+        }
+    }
+    return {next: next};
+}
+var baseTagList = ['FILE', 'DATE/TIME', 'LOT ID', 'SLOT', 'RECIPE', 'MEAS SET'];
+var extraTagList = ['WAFER ID', 'SITE', 'RCP CNT'];
+
+FXParser = function(text) {
+    this.reader = csvReader(text);
+    this.headers = {};
+    this.tagList = baseTagList;
+};
+
+FXParser.prototype = {
+    readDateTime: function() {
+        for (var row = this.next(); row; row = this.next()) {
+            if (!row[0]) break;
+            if (row[0].indexOf('COLLECTION DATE/TIME:') >= 0) {
+                return row.length > 1 ? row[1] : "";
+            }
+        }
+        return "";
+    },
+
+    readMeasurements: function(slot) {
+        var meas = [];
+        var slot_begin = [slot];
+        for (var row = this.next(); row; row = this.next()) {
+            if (!row[0]) break;
+            meas.push(slot_begin.concat(row.slice(1, -3)));
+        }
+        return meas;
+    },
+
+    readSection: function() {
+        var info = {};
+        for (var row = this.next(); row; row = this.next()) {
+            var key = row[0];
+            if (key === "RESULT TYPE") {
+                var film_stack = info["MEAS SET"];
+                this.headers[film_stack] = row.slice(1);
+            } else if (this.tagList.indexOf(key) >= 0) {
+                info[key] = row[1];
+            } else if (key == "Site #") {
+                var meas = this.readMeasurements(info['SLOT']);
+                return {info: info, measurements: meas};
+            }
+        }
+    },
+
+    readAll: function() {
+        var result = {};
+        while (true) {
+            var section = this.readSection();
+            if (!section) break;
+            var info = section.info, meas = section.measurements;
+            var measSet = info["MEAS SET"];
+            if (result[measSet]) {
+                for (var i = 0; i < meas.length; i++) {
+                    result[measSet].push(meas[i]);
+                }
+            } else {
+                result[measSet] = meas;
+            }
+        }
+        return result;
+    },
+
+    next: function() { return this.reader.next(); }
+};
